df870460c56a6349633be9e6f558ff918f4e4eca
juansmacias <juansmacias@gmail.com>
2022-12-01 07:48:07 -0500

    changes in api to return full user after create answers or update answers. Create new endpoint to get answers by hero id

--- api/src/plugins/heros.ts
+++ api/src/plugins/heros.ts
@@ -38,7 +38,20 @@ const herosPlugin = {
                     mode:'optional'
                 }
             }
-        }])
+        },{
+            method: 'GET',
+            path: '/heros/{heroId}/answers',
+            handler: getAnswersByHeroId,
+            options: {
+                auth:{
+                    mode:'optional',
+                },validate:{
+                    params: Joi.object({
+                        heroId: Joi.number().integer(),
+                    })
+                }
+            },
+        },])
     }
 }
 
@@ -86,5 +99,29 @@ async function GetAnswersFromAssignedHeros(request: Hapi.Request, h: Hapi.Respon
         return takenHeros.length>0?h.response(takenHeros).code(200):h.response(takenHeros).code(204)
     } catch (e) {
         return Boom.badImplementation("Failed to AnswersFromAssignedHeros"+e)
+    }
+}
+
+async function getAnswersByHeroId(request: Hapi.Request, h:Hapi.ResponseToolkit){
+    const { prisma } = request.server.app
+    const heroId = parseInt(request.params.heroId,10)
+
+    try {
+        const user = await prisma.user.findFirst({
+            where:{
+                heroId:heroId
+            },
+            select:{
+                answers:true
+            }
+        })
+
+        if(user){
+            return user.answers?.length>0?h.response(user.answers!).code(200):h.response([]).code(204)
+        } else {
+            return h.response([]).code(204)
+        }
+    } catch (e) {
+        return Boom.badImplementation("Failed to getAnswersByHeroId"+e)
     }
 }
